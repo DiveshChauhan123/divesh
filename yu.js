4f6028703bc8aa8e28ec66c395280caf37e2f07a
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2023-08-28 21:58:05 +0530

    Add files via upload

---
+++ yu.js
@@ -0,0 +1,96 @@
+/*console.log("divesh");
+let js = "tres";
+console.log(js);
+let Firstname = "ou"
+console.log(Firstname);
+//variable name conventions
+//i have to complete javascript by septembr15
+true;
+console.log(true);
+let x = true;
+console.log(x);
+console.log(typeof "stri")
+x = 'yes';
+console.log(x);
+
+//in javascript value se pta lgta h datatype c++ ki taraah ni ki variable se lge pta
+/*const y = 9;
+y = 10;
+console.log(y)*/
+//let is used  to change later but const does not change the value
+/*const u = 2037 - 1982;
+console.log(u)
+console.log(u * 5);
+console.log("divesh" + " " + "singh" + "chauhan")
+const now = 2037;
+const agejonas = now - 1991;
+const agesarah = now - 2018;
+console.log(now - 1991 > now - 2018)
+console.log(3 * 4 / 2 + 5)
+const firstname = 'divesh'
+const job = 'student'
+const year = 2023;
+const birthyear = 2004;
+console.log('i m' + firstname + ',a' + (year - birthyear) + ' year old ' + job);*/
+//const name = 'divesh'
+/*const ty = `i'm ${name}`;
+console.log(ty);
+console.log(`just a regular string..${ty}`)
+console.log(`i
+m
+divesh`)
+const firstname = 'divesh';
+const lastname = 'chauhan';
+console.log(`my name is ${firstname}`);
+console.log("hello")*/
+
+/*function calcagel(birthyear) {
+    const age = 2037 - birthyear;
+    console.log(age)
+}
+calcagel(2009)
+const cal = function (birthyear) {
+    return 2037 - birthyear;
+}
+console.log(cal)*/
+//arrow functionconst birthyear =90
+/*const y=birthyear=>2048-birthyear;
+const yu=y(90)
+console.log(yu);*/
+/*const friend1='michael'
+const friend2='steven'
+const friend3='peter';
+const friends=['michael','steven','peter']  
+console.log(friends)
+const years=new Array(1991,19992,1999,3990)
+console.log(years);
+console.log(friends[0],friends[1],friends[2]);
+console.log(friends.length)*/
+//built in arrray methods
+//push method add no or string to the array
+//
+/*
+const friends=['divesh','rahul','rizul']
+friends.push('lokesh')
+console.log(friends)
+friends.unshift('john')
+console.log(friends)
+friends.pop()
+console.log(friends)
+//unshift add the elements to the starting of the array
+friends.shift()
+friends.shift()
+console.log(friends)
+*/
+// prompt("what is your name??")
+
+/*"use strict"
+const x='23';
+if(x===23){
+    console.log("hello");
+}*/
+const deepak=90;
+const rahul=deepak=>{
+    console.log(deepak);
+}
+rahul(deepak);
